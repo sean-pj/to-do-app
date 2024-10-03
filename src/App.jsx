e92fdc4b236fb953975b41b590064d8f05ea6989
Sean Murphy <sean.peterjames.murphy@gmail.com>
2024-10-03 10:08:38 -0400

    Tasks view

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Button from 'react-bootstrap/Button';
-
+// import Button from 'react-bootstrap/Button';
+import TaskList from './components/TaskList/TaskList';
 
 function App() {
-
   return (
     <>
       <h1>To-Do App</h1>
-      <Button variant="primary">Hello</Button>
+      <TaskList></TaskList>
     </>
   )
 }
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,11 @@
+function TaskList() {
+    return (
+        <>
+        <li>Foo</li>
+        <li>Bar</li>
+        <li>Baz</li>
+        </>
+    )
+}
+
+export default TaskList
