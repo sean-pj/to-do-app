b12ee05e3f3f6a69bd50be2dd32aba2a4d5c9783
Sean Murphy <sean.peterjames.murphy@gmail.com>
2024-10-03 09:59:41 -0400

    Create React app

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from 'react-bootstrap/Button';
+
+
+function App() {
+
+  return (
+    <>
+      <h1>To-Do App</h1>
+      <Button variant="primary">Hello</Button>
+    </>
+  )
+}
+
+export default App
