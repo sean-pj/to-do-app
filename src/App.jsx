6a782bffa5aad0d40cf762fff11f220444947a24
Sean Murphy <sean.peterjames.murphy@gmail.com>
2024-10-10 12:48:37 -0400

    TaskList react-boostrap styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Container, Row, Col} from "react-bootstrap";
 import './App.css'
-// import Button from 'react-bootstrap/Button';
 import TaskList from './components/TaskList/TaskList';
 
 function App() {
+  const style = {
+    backgroundColor: "#242424",  // Make sure this is a valid CSS color
+  };
+
   return (
-    <>
-      <h1>To-Do App</h1>
-      <TaskList></TaskList>
-    </>
-  )
+    <Container style={style} className="vh-100 vw-100 d-flex justify-content-center align-items-center">
+    <Row className='w-100'>
+      <Col >
+      <div>
+        <h1>To-Do App</h1>
+        <TaskList></TaskList>
+      </div>
+      </Col>
+    </Row>
+    </Container>
+  );
 }
 
 export default App
