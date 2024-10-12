49bc935f9d4fd5552eb3cdbbf5c83f95e7cf1350
Sean Murphy <sean.peterjames.murphy@gmail.com>
2024-10-12 13:59:26 -0400

    Due date badges

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,23 +1,31 @@
-import { Accordion, ListGroup, Container, Row, Col, ListGroupItem} from "react-bootstrap";
+import { Accordion, Badge, ListGroup, Container, Row, Col, ListGroupItem} from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
+function ToDoBadge({dueDate}) {
+    return (
+        <Badge style={{marginLeft: "1rem"}} bg="secondary">{dueDate}</Badge>
+    );
+}
 
 function TaskList() {
+    const [todos, setTodos] = useState([
+        {header: "Just some demo tasks", description: "Description 1", due: "Monday"}, 
+        {header: "As an example", description: "Description 2", due: "Tuesday"}
+    ]);
+    
 
     return (
         //defaultActiveKey="0"
         <Accordion className='w-25 mx-auto' data-bs-theme="dark">
-            <Accordion.Item eventKey="0"> 
-                <Accordion.Header>Example Task #1</Accordion.Header>
+            {todos.map((todo, index) => (
+                <Accordion.Item key={index} eventKey={String(index)}> 
+                <Accordion.Header>{todo.header} <ToDoBadge dueDate={todo.due} /> </Accordion.Header>
                 <Accordion.Body>
-                Description 2
+                {todo.description}
                 </Accordion.Body>
-            </Accordion.Item>
-            <Accordion.Item eventKey="1">
-                <Accordion.Header>Example Task #2</Accordion.Header>
-                <Accordion.Body>
-                Description 1
-                </Accordion.Body>
-            </Accordion.Item>
+                </Accordion.Item>
+            ))}
         </Accordion>
     )
 }
